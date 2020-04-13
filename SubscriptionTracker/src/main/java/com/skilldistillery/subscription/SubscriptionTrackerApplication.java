0ef262dbb08cf5aa7da82b340ad7063814299e10
Frank Martin <cedricmrtn@gmail.com>
2020-04-13 10:00:51 -0500

    updated db name

--- SubscriptionTracker/src/main/java/com/skilldistillery/subscription/SubscriptionTrackerApplication.java
+++ SubscriptionTracker/src/main/java/com/skilldistillery/subscription/SubscriptionTrackerApplication.java
@@ -2,9 +2,15 @@ package com.skilldistillery.subscription;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SubscriptionTrackerApplication {
+public class SubscriptionTrackerApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SubscriptionTrackerApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SubscriptionTrackerApplication.class, args);
